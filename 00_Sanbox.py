8f89a3954a1510a9076bf3c32949c6a03c91a1b3
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2023-02-23 10:40:37 +1300

    Add files via upload

--- 00_Sanbox.py
+++ 00_Sanbox.py
@@ -1,10 +1,13 @@
 # Ask users for a number
-get_number = int(input("choose a number"))
+get_number = int(input("choose a number "))
 
 # Multiply the number by 5
 times_five = get_number * 5
 
+answer = "{} times five is equal to " \
+         "{}".format(get_number, times_five)
+
 # Output the result
-print(times_five)
+print(answer)
 
 

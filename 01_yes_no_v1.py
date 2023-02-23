f211f32856eb16cea77b815bf3b90457868f05a1
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2023-02-24 09:58:34 +1300

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,7 @@
+# Ask the user if they have played before
+show_instructions = input("have you played this game"
+                          "before?")
+
+# If they say yes, output 'program continues'
+
+# If they say no, output 'display instruction'

7fbb1db8828ccf0f97f85d7cbdec6764b89bd0b3
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2023-03-22 12:29:51 +1300

    Add files via upload

--- 00_LU_base_v_01.py
+++ 00_LU_base_v_01.py
@@ -1,3 +1,6 @@
+import random
+
+
 # Functions go here
 
 # Checks user answer yes / no to a question
@@ -58,5 +61,54 @@ print("program continues")
 
 # Ask user how much the want to play with
 how_much = num_check("How much would you like to play with? ", 0, 10)
-print(f'You chose ${how_much:.2f}')
 
+balance = how_much
+
+rounds_played = 0
+
+play_again = input("press <enter> to play...").lower()
+while play_again == "":
+
+    # increase # of rounds played
+    rounds_played += 1
+
+    # print round number
+    print()
+    print(f'*** round #{rounds_played} ***')
+    chosen_num = random.randint(1, 100)
+
+    # Adjust balance
+    # if the random # is between 1 and 5,
+    # user gets a unicorn (add $4 to balance)
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+    # if the random # is between 6 and 36
+    # user gets a zebra (subtract $1 from balance)
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+
+    # The token is either horse or zebra...
+    # in both cases, subtract %0.50 from the balance
+    else:
+        # if the number is even, set the chosen
+        # item to a horse
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        # otherwise set it to a zebra
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+
+    print(f'You got a {chosen}, your balance is ${balance:.2f}')
+
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+        play_again = input("Press Enter to play again or 'xxx' to quit ")
+
+print()
+print(f'Final Balance: ${balance:.2f}')
+print("Thanks for playing")
--- 06_Rounds_v02.py
+++ 06_Rounds_v02.py
@@ -1,3 +1,5 @@
+import random
+
 # set balance for testing purposes
 balance = 5
 
@@ -12,9 +14,33 @@ while play_again == "":
     # print round number
     print()
     print(f'*** round #{rounds_played} ***')
-    balance -= 1
-    print("balance: ", balance)
-    print()
+    chosen_num = random.randint(1, 100)
+
+    # Adjust balance
+    # if the random # is between 1 and 5,
+    # user gets a unicorn (add $4 to balance)
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+    # if the random # is between 6 and 36
+    # user gets a zebra (subtract $1 from balance)
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+
+    # The token is either horse or zebra...
+    # in both cases, subtract %0.50 from the balance
+    else:
+        # if the number is even, set the chosen
+        # item to a horse
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        # otherwise set it to a zebra
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+
+    print(f'You got a {chosen}, your balance is ${balance:.2f}')
 
     if balance < 1:
         play_again = "xxx"
